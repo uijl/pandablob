97ebd1a9d8053a6f7ce8651bba18cd93eccb5df9
Joris den Uijl <jorisdenuijl@gmail.com>
2020-08-30 18:31:32 +0200

    Added testing additional kwargs for upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,21 +29,38 @@ def pandas_arguments_download():
 
 
 @pytest.fixture()
+def pandas_arguments_upload():
+    """Return a dict with the keyword arguments for pandas."""
+
+    return {
+        "csv": {"header": ["one", "two"]},
+        "json": {"orient": "index"},
+        "txt": {"header": ["one", "two"]},
+        "xls": {"header": ["one", "two"]},
+        "xlsx": {"header": ["one", "two"]},
+    }
+
+
+@pytest.fixture()
 def dataframe_upload():
     """Return a dict with the keyword arguments for pandas."""
 
-    def _make_dataframe(file_extension, file, stream=False):
+    def _make_dataframe(file_extension, file, additional_kwargs=None, stream=False):
         """Make a pandas DataFrame."""
+
+        if not additional_kwargs:
+            additional_kwargs = {}
 
         if file_extension in [".csv", ".txt"]:
             arguments = {"index_col": 0, "delimiter": ",", "float_precision": "high"}
+            arguments.update({key: value for key, value in additional_kwargs.items()})
             if not stream:
                 return pd.read_table(file, **arguments)
             return pd.read_table(io.StringIO(file), **arguments)
         elif file_extension in [".xls", ".xlsx"]:
-            return pd.read_excel(file, index_col=0)
+            return pd.read_excel(file, index_col=0, **additional_kwargs)
         elif file_extension == ".json":
-            return pd.read_json(file)
+            return pd.read_json(file, **additional_kwargs)
 
     return _make_dataframe
 
---
+++ tests/test_upload_kwargs.py
@@ -0,0 +1,41 @@
+"""Test uploading a DataFrame to a specified a blob with given kwargs."""
+
+import io
+
+import pandas as pd
+import pytest
+
+import pandablob
+
+
+@pytest.mark.parametrize("file", ["csv", "json", "txt", "xls", "xlsx"])
+def test_upload(
+    file, test_files, dataframe_upload, pandas_arguments_upload, mock_upload
+):
+    """Mock uploading to the azure blob."""
+
+    # Create required input
+    file_name = f"test_data.{file}"
+    file_location = test_files.joinpath(file_name)
+    extension = file_location.suffix
+
+    # Get mock object from fixture
+    MockAzureBlob = mock_upload(file_name)
+
+    # Make DataFrame to upload
+    df = dataframe_upload(extension, file_location)
+    pandablob.df_to_blob(df, MockAzureBlob, pandas_arguments_upload[file])
+
+    # Mock uploading the DataFrame
+    pandablob_stream = MockAzureBlob.upload_blob.call_args[0][0]
+
+    # Test result
+    if extension == ".json":
+        result_df = dataframe_upload(
+            extension, pandablob_stream, pandas_arguments_upload[file], True
+        )
+    else:
+        result_df = dataframe_upload(extension, pandablob_stream, stream=True)
+        df.columns = ["one", "two"]
+
+    assert df.equals(result_df)
